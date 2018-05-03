5bc71f0cd8579340c1dc0ae3501bf9a8772e9758
Cherish <shmilycherish@gmail.com>
2018-05-03 20:40:35 +0800

    add another test to make marsrover has some attributes

--- src/main/java/com/studyforever/tdd/MarsRover.java
+++ src/main/java/com/studyforever/tdd/MarsRover.java
@@ -1,10 +1,19 @@
 package com.studyforever.tdd;
 
+import static java.lang.String.format;
+
 public class MarsRover {
+    private final int x;
+    private final int y;
+    private final String direction;
+
     public MarsRover(int x, int y, String direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
     }
 
     public String getStatus() {
-        return "0,0,N";
+        return format("%s,%s,%s", x, y, direction);
     }
 }
--- src/test/java/com/studyforever/tdd/MarsRoverTest.java
+++ src/test/java/com/studyforever/tdd/MarsRoverTest.java
@@ -7,11 +7,20 @@ import static org.junit.Assert.assertThat;
 
 public class MarsRoverTest {
     @Test
-    public void shouldReturnMarsRoverStatus() {
+    public void shouldReturn00NWhenMarsRoverStatusIS00N() {
         MarsRover marsrover = new MarsRover(0, 0, "N");
 
         String status = marsrover.getStatus();
 
         assertThat(status, is("0,0,N"));
+    }
+
+    @Test
+    public void shouldReturn01WWhenMarsRoverStatusIS01W() {
+        MarsRover marsrover = new MarsRover(0, 1, "W");
+
+        String status = marsrover.getStatus();
+
+        assertThat(status, is("0,1,W"));
     }
 }
